5da7158e677e2edff1352b69f9de5e79a9122bde
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-09-29 15:33:30 +0400

    Added 'required' argument to the RepositoryForm.

--- django_vcs_watch/forms.py
+++ django_vcs_watch/forms.py
@@ -4,6 +4,7 @@ from models import Repository
 class RepositoryForm(forms.ModelForm):
     password = forms.CharField(
             max_length=Repository._meta.get_field('password').max_length,
+            required=(not Repository._meta.get_field('password').blank),
             widget=forms.PasswordInput())
 
     class Meta:
