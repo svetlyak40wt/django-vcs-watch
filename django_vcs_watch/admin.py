18fbb33bc00cd7f39a2b15917bc76f55610b2017
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-09-29 15:08:13 +0400

    Link to repository page from the admin interface.

--- django_vcs_watch/admin.py
+++ django_vcs_watch/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from models import Repository
 
 class RepositoryAdmin(admin.ModelAdmin):
-    list_display = ('hash', 'url', 'last_rev')
+    list_display = ('hash', 'url_link', 'last_rev')
+
+    def url_link(self, instance):
+        return '<a href="%s">%s</a>' % (instance.get_absolute_url(), instance.url)
+    url_link.allow_tags = True
 
 admin.site.register(Repository, RepositoryAdmin)
 
