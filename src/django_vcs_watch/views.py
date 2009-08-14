c6aa7cbae32e87c41fd81ceae30bd4ed76014bae
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-08-14 15:24:52 -0400

    Added redirect to existing repository from add view.

--- src/django_vcs_watch/views.py
+++ src/django_vcs_watch/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic.list_detail import object_detail, object_list
+from django.views.generic.create_update import create_object
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, HttpResponseRedirect
 
-from models import Repository
+from django_vcs_watch.models import Repository
 
 def revision(request, repository_slug, revision):
     repository = get_object_or_404(Repository, hash=repository_slug)
@@ -13,9 +14,20 @@ def revision(request, repository_slug, revision):
             slug=revision,
             slug_field='rev')
 
+
 @login_required
 def profile(request):
     return object_list(
             request,
             queryset=request.user.repository_set.all())
+
+
+def add_repository(request, *args, **kwargs):
+    url = request.POST.get('url', None)
+    reps = Repository.objects.filter(url = url).all()
+
+    if len(reps) > 0:
+        return HttpResponseRedirect(reps[0].get_absolute_url())
+
+    return create_object(request, *args, **kwargs)
 
